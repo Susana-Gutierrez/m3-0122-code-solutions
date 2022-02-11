6fba771af178d0b0f517acec595eb61776ac3d75
Susana Gutierrez <susanagabriela_ap@hotmail.com>
2022-02-10 21:21:13 -0800

    Completed react-form-controls exercise.

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSumit = this.handleSumit.bind(this);
+    this.state = { email: '' };
+
+  }
+
+  handleChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handleSumit(event) {
+    console.log('this.state: ', this.state);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSumit}>
+        <label>Email:</label>
+        <input type="text" name="email" value={this.state.value} onChange={this.handleChange} />
+        <button>Sign Up</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<NewsletterForm />, document.getElementById('root'));

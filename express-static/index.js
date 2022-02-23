331ba60c0e6106d7e56c91bbe98d8a13c01cf675
Susana Gutierrez <susanagabriela_ap@hotmail.com>
2022-02-22 20:58:52 -0800

    Changes completed express-static.

--- express-static/index.js
+++ express-static/index.js
@@ -2,15 +2,13 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+var absPath = path.join(__dirname, 'public');
+console.log('Absolute Path: ', absPath);
+
+var publicDir = express.static(absPath);
+app.use(publicDir);
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Express server listening on port 3000');
 });
-
-var absPath = path.join(__dirname, 'public');
-
-console.log('Absolute Path: ', absPath);
-
-var publicDir = express.static('public');
-
-app.use(publicDir);
